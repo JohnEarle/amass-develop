23660becf1148e4b30b6ef4c3197152636fc92ce
John Earle <jcae6@hotmail.com>
2024-12-24 01:04:26 -0500

    Update load.go

--- engine/plugins/load.go
+++ engine/plugins/load.go
@@ -54,7 +54,7 @@ var pluginNewFuncs = []func() et.Plugin{
 	scrape.NewIPVerse,
 	scrape.NewRapidDNS,
 	scrape.NewSiteDossier,
-	scrape.LetItGo,
+	scrape.NewLetItGo,
 	whois.NewWHOIS,
 	NewIPNetblock,
 	NewJARMFingerprints,
